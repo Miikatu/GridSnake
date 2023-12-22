4c2aa3be71ff8a094c914f63f145b33d1dd8a1ac
miikatu <miikatti@gmail.com>
2023-12-22 04:19:22 +0200

    Initial commit

---
+++ CMakeFiles/GridSnake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GridSnake.dir/src/Main.cpp.o"
+  "GridSnake"
+  "GridSnake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GridSnake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/GridSnake.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GridSnake.
+# This may be replaced when dependencies are built.
